9527210323f8ded0c9061451d3ac4c130d46d55e
Shamim <tshamimbd@gmail.com>
2026-07-18 23:10:22 +0600

    feat: add Meta Pixel (ID: 4408949989215502) tracking code

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -112,8 +112,39 @@ export default class extends Document {
               rel="stylesheet"
             />
           </noscript>
+
+          {/* Meta Pixel Code */}
+          <script
+            id="meta-pixel"
+            dangerouslySetInnerHTML={{
+              __html: `
+                !function(f,b,e,v,n,t,s)
+                {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+                n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+                if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+                n.queue=[];t=b.createElement(e);t.async=!0;
+                t.src='https://connect.facebook.net/en_US/fbevents.js';
+                s=b.getElementsByTagName(e)[0];
+                s.parentNode.insertBefore(t,s);
+                }(window, document,'script');
+                fbq('init', '4408949989215502');
+                fbq('track', 'PageView');
+              `,
+            }}
+          />
+          {/* End Meta Pixel Code */}
         </Head>
         <body>
+          {/* Meta Pixel noscript fallback */}
+          <noscript>
+            <img
+              height="1"
+              width="1"
+              style={{ display: 'none' }}
+              src="https://www.facebook.com/tr?id=4408949989215502&ev=PageView&noscript=1"
+              alt=""
+            />
+          </noscript>
           {/* google tag manager was here */}
           <Main />
           <NextScript />
